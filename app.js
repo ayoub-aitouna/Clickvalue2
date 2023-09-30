c3e19e895c0de0cd591b65bce9667b49cb7ee759
jrayoub <aitounaayoub05@gmail.com>
2023-09-30 01:02:47 +0100

    fixed cursor

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 let circle = document.getElementById('circle');
 var y;
 var x;
-const onMouseMove = (e) => {
 
-    x = -5 + e.pageX;
-    y = -5 + e.pageY;
-    circle.style.transform = "translate(" + x + "px, " + (y) + "px)"
-}
+const updatePosition = () => {
+    circle.style.transform = "translate(" + x + "px, " + y + "px)";
+};
+
+const onMouseMove = (e) => {
+    const scrollX = window.scrollX || window.pageXOffset; // Horizontal scroll position
+    const scrollY = window.scrollY || window.pageYOffset; // Vertical scroll position
+    x = e.pageX - circle.clientWidth / 2 - scrollX;
+    y = e.pageY - circle.clientHeight / 2 - scrollY;
+    updatePosition();
+};
 
 document.addEventListener('mousemove', onMouseMove);
